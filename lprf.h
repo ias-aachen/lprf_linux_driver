f77560a92ad3fc6a8457dd77fdd0e1a1caeaacf7
Tilman Sinning <tilman.sinning@rwth-aachen.de>
2016-11-08 17:00:40 +0100

    initial commit

---
+++ lprf.h
@@ -0,0 +1,32 @@
+/*
+ * IAS LPRF driver
+ *
+ * Copyright (C) 2015 IAS RWTH Aachen
+ * Adapted from  AT86RF230 driver, Copyright (C) 2009-2012 Siemens AG
+ * Also based on scull driver from "Linux Device Drivers"
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ *
+ * Written by:
+ * Tilman Sinning <tilman.sinning@rwth-aachen.de
+ * Moritz Schrey <mschrey@ias.rwth-aachen.de>
+ * Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
+ * Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
+ * Alexander Aring <aar@pengutronix.de>
+ * Alessandro Rubini
+ * Jonathan Corbet
+ */
+
+#ifndef _LPRF_H_
+#define _LPRF_H_
+
+
+
+#endif // _LPRF_H_
---
+++ lprf_rx.c
@@ -0,0 +1,43 @@
+/* this is a mixture from at86rf230.c and scull-main
+ * all references to at86rf230 are changed to lprf
+ * IAS LPRF driver
+ *
+ * Copyright (C) 2015 IAS RWTH Aachen
+ * Adapted from  AT86RF230 driver, Copyright (C) 2009-2012 Siemens AG
+ * Also based on scull driver from "Linux Device Drivers"
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ *
+ * Works on kernel version 4.4.15+
+ *
+ * Written by:
+ * Tilman Sinning <tilman.sinning@rwth-aachen.de>
+ * Moritz Schrey <mschrey@ias.rwth-aachen.de>
+ * Jan Richter-Brockmann <jan.richter-brockmann@rwth-aachen.de>
+ * Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
+ * Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
+ * Alexander Aring <aar@pengutronix.de>
+ * Alessandro Rubini
+ * Jonathan Corbet
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/gpio.h>
+#include <linux/delay.h>
+#include <linux/spi/spi.h>
+#include <linux/regmap.h>
+#include <linux/of_gpio.h>
+#include <linux/ieee802154.h>
+#include <linux/debugfs.h>
+
+#include <net/mac802154.h>
+#include <net/cfg802154.h>
+
+#include "lprf.h"
