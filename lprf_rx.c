88b047879de1da70d4a4aa6c2ca08d4697dd902b
Tilman Sinning <tilman.sinning@rwth-aachen.de>
2016-11-14 18:12:30 +0100

    Add basic SPI functionality

--- lprf_rx.c
+++ lprf_rx.c
@@ -41,22 +41,93 @@
 #include <net/cfg802154.h>
 
 #include "lprf.h"
+#include "lprf_registers.h"
 
+
+struct lprf_platform_data {
+	int some_custom_value;
+};
+
+
+static int lprf_probe(struct spi_device *spi)
+{
+	u32 custom_value = 0;
+	int ret = 0;
+	struct lprf_platform_data *pdata = 0;
+	uint8_t *tx_buf = 0;
+	uint8_t *rx_buf = 0;
+	uint8_t chip_id_h = 0;
+	PRINT_DEBUG( "call lprf_probe\n");
+
+
+	pdata = spi->dev.platform_data;
+
+	// Get platform data
+	if (!IS_ENABLED(CONFIG_OF) || !spi->dev.of_node) {
+		if (!pdata)
+			return -ENOENT;
+	}
+
+	PRINT_DEBUG( "successfully parsed platform data\n");
+
+
+	ret = of_property_read_u32(spi->dev.of_node, "some-custom-value", &custom_value);
+	PRINT_DEBUG( "returned value:\t%d, custom value:\t%d\n", ret, custom_value);
+
+	rx_buf = kmalloc(1, GFP_KERNEL);
+	tx_buf = kmalloc(2, GFP_KERNEL);
+
+	rx_buf[0] = 0;
+
+	tx_buf[0] = 0x80;
+	tx_buf[1] = RG_CHIP_ID_H;
+
+	spi_write_then_read(spi, tx_buf, 2, rx_buf, 1);
+
+	chip_id_h = rx_buf[0];
+	tx_buf[1] = RG_CHIP_ID_L;
+
+	spi_write_then_read(spi, tx_buf, 2, rx_buf, 1);
+
+	PRINT_DEBUG( "LPRF Chip found with chip ID %x\n", (chip_id_h << 8) + rx_buf[0]);
+
+	kfree(rx_buf);
+	kfree(tx_buf);
+	return 0;
+}
+
+static int lprf_remove(struct spi_device *spi)
+{
+	PRINT_DEBUG( "call lprf_remove\n");
+	return 0;
+}
+
+static const struct of_device_id lprf_of_match[] = {
+	{ .compatible = "ias,lprf", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, lprf_of_match);
+
+static const struct spi_device_id lprf_device_id[] = {
+	{ .name = "lprf", },
+	{ },
+};
+MODULE_DEVICE_TABLE(spi, lprf_device_id);
+
+static struct spi_driver lprf_driver = {
+	.id_table = lprf_device_id,
+	.driver = {
+		.of_match_table = of_match_ptr(lprf_of_match),
+		.name	= "lprf",
+	},
+	.probe      = lprf_probe,
+	.remove     = lprf_remove,
+};
+
+module_spi_driver(lprf_driver);
 
 MODULE_DESCRIPTION("LPRF RX Driver");
 MODULE_LICENSE("GPL v2");
 
 
-static int hello_init(void)
-{
-	PRINT_DEBUG("Call init function\n");
-	return 0;
-}
 
-static void hello_exit(void)
-{
-	PRINT_DEBUG("Call exit function\n");
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
